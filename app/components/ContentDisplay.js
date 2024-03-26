3ad111c4a73168ad9922efae01396554c629e6c2
fozlur-rahman <fozlurrahman993@gmail.com>
2024-03-26 14:09:04 +0600

    solve the provlem

--- app/components/ContentDisplay.js
+++ app/components/ContentDisplay.js
@@ -1,22 +1,20 @@
 import { getDocumentContent } from "@/lib/docs";
 import Link from "next/link";
 
-
-
 const ContentDisplay = async ({ id }) => {
     const documentContent = await getDocumentContent(id);
 
     return (
         <article className="prose dark:prose-invert">
             <h1>{documentContent.title}</h1>
-            <div>
-                <span>Published On: {documentContent.date}</span> by
+            <div className="flex items-center gap-2 mt-0">
+                <span>Published On: {documentContent.date}</span>
                 <Link href={`/author/${documentContent.author}`}>
-                    <p>{documentContent.author}</p>
+                    {documentContent.author}
                 </Link>
                 under the
                 <Link href={`/category/${documentContent.category}`}>
-                    <p>{documentContent.category}</p>
+                    {documentContent.category}
                 </Link>
                 category.
             </div>
@@ -26,9 +24,9 @@ const ContentDisplay = async ({ id }) => {
                         <Link
                             key={tag}
                             href={`/tags/${tag}`}
-                            className="  mt-0 mb-0 bg-sky-300 px-2 rounded-full"
+                            className=" mt-0 mb-0 bg-sky-200 px-2  rounded-full"
                         >
-                            <p>{tag}</p>
+                            {tag}
                         </Link>
                     ))}
             </div>
